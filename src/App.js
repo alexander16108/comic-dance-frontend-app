0fbb334d6b1a40315198ea7cdf420c602b218d4a
alexander16108 <alexandermayowa05@gmail.com>
2022-03-28 16:58:27 +0100

    fixing linter errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import NavBar from './components/navbar/NavBar';
 // import Apartments from './pages/Apartments';
