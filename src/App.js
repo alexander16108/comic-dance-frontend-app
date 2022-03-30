9ffa930bf6836d3cf1bd6da7c118d302161ad0ca
alexander16108 <alexandermayowa05@gmail.com>
2022-03-30 08:34:22 +0100

    fixing linter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar/NavBar';
-import DanceClasses from './pages/EnrolledClasses';
+import DanceClasses from './pages/DanceClass';
 import EnrollmentForm from './pages/EnrollmentForm';
-import EnrolledForm from './pages/EnrolledClasses';
+import EnrolledClasses from './pages/EnrolledClasses';
 import DeleteClasses from './pages/DeleteClasses';
 
 const App = () => (
@@ -13,13 +13,13 @@ const App = () => (
       <Route exact path="/" element={<DanceClasses />} />
     </Routes>
     <Routes>
-      <Route exact path="/lease_form" element={<EnrollmentForm />} />
+      <Route exact path="/Enrollment_Form" element={<EnrollmentForm />} />
     </Routes>
     <Routes>
-      <Route exact path="/my_leases" element={<EnrolledForm />} />
+      <Route exact path="/Enrolled_Classes" element={<EnrolledClasses />} />
     </Routes>
     <Routes>
-      <Route exact path="/delete_leases" element={<DeleteClasses />} />
+      <Route exact path="/Delete_Classes" element={<DeleteClasses />} />
     </Routes>
   </Router>
 );
