78b3f528cc4a792a99ed2f0bd35c68ebfc32acfe
alexander16108 <alexandermayowa05@gmail.com>
2022-03-31 11:13:53 +0100

    Create a lease form

--- src/pages/EnrollmentForm.js
+++ src/pages/EnrollmentForm.js
@@ -1,7 +1,39 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
-const EnrollmentForm = () => (
-  <h1 className="random">EnrollmentForm</h1>
-);
+const EnrollmentForm = () => {
+  const details = {
+    heading: 'Book a Dance Lesson/Class',
+    text: 'There are 15 different Dance Classes and Lessons listed',
+  };
+
+  return (
+    <div className="EnrollmentForm-form">
+      <h1>{details.heading}</h1>
+      <hr />
+      <p>{details.text}</p>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="textInput" className="form-label">Name</label>
+          <input type="text" id="textInput" className="form-control" placeholder="Your name" value="User name" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="select" className="form-label">Dance Class</label>
+          <select id="select" className="form-select">
+            <option selected>Selected Dance Class</option>
+            <option>Wavy</option>
+          </select>
+        </div>
+        {/* <div className="mb-3">
+          <label htmlFor="select" className="form-label"> Dance</label>
+          <select id="select" className="form-select">
+            <option>Lusaka</option>
+          </select>
+        </div> */}
+        <button type="submit" className="btn btn-primary">Book Now</button>
+      </form>
+    </div>
+  );
+};
 
 export default EnrollmentForm;
