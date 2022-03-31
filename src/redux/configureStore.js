90b743c24a4d1ef9a08926aa65841f3232bb07c2
alexander16108 <alexandermayowa05@gmail.com>
2022-03-31 13:34:44 +0100

    Create an enrollment reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,8 +2,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { userReducer } from './user/user';
+import { EnrollmentReducer } from '../enroll/enroll';
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({
+  user: userReducer,
+  EnrollmentReducer,
+});
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
