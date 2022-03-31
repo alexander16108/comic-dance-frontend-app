6cbcdb45fa47a1278aa3fc8201a96860ac0be382
alexander16108 <alexandermayowa05@gmail.com>
2022-03-31 21:08:47 +0100

    Add signup, logout reducer test

--- src/tests/authentication_reducers/authReducers.test.js
+++ src/tests/authentication_reducers/authReducers.test.js
@@ -15,4 +15,30 @@ describe('post reducer', () => {
       signedUp: true,
     });
   });
+
+  it('Signs up properly', () => {
+    expect(hitAPIWithSignupDetails({
+      name: 'Henry',
+      email: 'henry@gmail.com',
+      password: 'kkkkkk',
+    })).toEqual({
+      name: 'Henry',
+      email: 'henry@gmail.com',
+      loggedIn: '',
+      userId: 1,
+      signedUp: true,
+    });
+  });
+
+  it('Logs out properly', () => {
+    expect(hitAPIWithLogoutDetails({
+      auth: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNSIsInNjcCI6InVzZXIiLCJhdWQiOm51bGwsImlhdCI6MTY0NjQ5MDI3OSwiZXhwIjoxNjQ2NDkzODc5LCJqdGkiOiIyZTgzOWM1MC1hZTEwLTQ1NjUtYmY1MC04YzMzMzE2ZDQ2ODYifQ.KdGYxJUtaU7Ut9327pyTuYf2xy9oI1rJfs9qjRs8FhM',
+    })).toEqual({
+      name: '',
+      email: '',
+      loggedIn: 'out',
+      userId: '',
+      signedUp: false,
+    });
+  });
 });
