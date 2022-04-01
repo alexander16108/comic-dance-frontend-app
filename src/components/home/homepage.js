5a90ea999f9bfc3e72facb369c650aad7d174a20
alexander16108 <alexandermayowa05@gmail.com>
2022-04-01 13:24:08 +0100

    Setup the home function using the arrow function in homepage.js

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+const Home = () => {
+  const classess = useSelector((state) => state.class);
+  const { classs } = classess;
+  console.log(classs, 'hello');
+  if (!classs[0]) {
+    return (
+      <h1>Loading</h1>
+    );
+  }
+};
