6d88af78dc1b18b21cada1ca63bd8596d2c0ece8
alexander16108 <alexandermayowa05@gmail.com>
2022-04-03 23:08:06 +0100

    updating enrollment apis

--- src/helpers/links.js
+++ src/helpers/links.js
@@ -5,18 +5,6 @@ const links = [
     text: 'DANCE CLASSES',
     name: 'dance_classes',
   },
-  {
-    id: 2,
-    path: '/enrollment_form',
-    text: 'FORM',
-    name: 'enroll_form',
-  },
-  {
-    id: 3,
-    path: '/my _classes',
-    text: 'RESERVATIONS',
-    name: 'my_classes',
-  },
 ];
 
 export default links;
--- src/pages/ClassDetails.js
+++ src/pages/ClassDetails.js
@@ -5,5 +5,107 @@ import { getSingleEnrollmentAction } from '../redux/enroll/enroll';
 import { login } from '../redux/user/user';
 import persistLogin from '../helpers/persistLogin';
 
+const MyClassDetails = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const { classDetails, user } = useSelector((state) => state);
+  const { classs, loading, err: error } = classDetails;
+
+  useEffect(() => {
+    if (user.userId) {
+      dispatch(getSingleEnrollmentAction(params.lease_id, user.userId));
+    }
+  }, [user]);
+
+  useEffect(() => {
+    persistLogin(login, dispatch);
+  }, []);
+
+  return (
+    classs ? (
+      <div className="container">
+        {error && (
+        <p className="alert alert-danger">
+          {error}
+        </p>
+        ) }
+        {loading ? 'loading' : (
+          <div className=" card mb-3 mt-5">
+            <div className="row g-0">
+              <div className="col-md-5">
+                <img src={lease.apartmentDetails.image} className="h-100 img-fluid rounded-start" alt="..." />
+                {' '}
+
+              </div>
+              <div className="col-md-7">
+                <div className="card-body">
+                  <h3 className="card-title text-info text-center">
+                    {lease.apartmentDetails.name}
+                  </h3>
+                  <p className="text-center">
+                    {' '}
+                    {lease.apartmentDetails.description}
+                    {' '}
+                  </p>
+                  <table className="table">
+                    <tbody>
+                      <tr>
+                        <td />
+                        <td>City</td>
+                        <td> - </td>
+                        <td />
+                        <td>{lease.apartmentDetails.city}</td>
+                      </tr>
+                      <tr>
+                        <td />
+
+                        <td>Monthly Rent</td>
+                        <td> - </td>
+                        <td />
+                        <td>
+                          $
+                          {lease.apartmentDetails.monthly_rent}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td />
+                        <td>Maintenance Fee</td>
+                        <td> - </td>
+                        <td />
+                        <td>
+                          $
+                          {lease.apartmentDetails.maintenance_fee}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td />
+
+                        <td>Expiry Date</td>
+                        <td> - </td>
+                        <td />
+                        <td>{lease.apartmentDetails.reservation_expiry_date}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        <h4 className="text-center text-info  my-5">Interiors</h4>
+        <div className="interior-card">
+          { loading ? '' : (lease.apartmentDetails.interior.map((interior) => (
+            <div key={interior} className="col d-flex justify-content-center mb-3 row g-5">
+              <div className=" border bg-light h-80 w-100 ">
+                <img src={interior} className="img-fluid rounded-start h-100 w-100 align-center" alt="..." />
+              </div>
+            </div>
+          )))}
+        </div>
+      </div>
+    ) : <p>Please, wait...</p>
+  );
+};
 
 export default MyClassDetails;
